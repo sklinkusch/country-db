261a83ca44449c5baf78b4cac298e204018b676f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-03-20 21:09:01 +0100

    add h1 to site

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,11 @@ import AppContext from './AppContext';
 export default function Title() {
   return (
     <AppContext.Consumer>
-      {value => (<div>
+      {value => (<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
         <input placeholder="Filter countries" onChange={value.handleChange} />
+        <div style={{ width: 'calc(100% - 404px)', marginRight: '202px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContents: 'center' }}>
+          <h1 style={{ width: 'fit-content', textAlign: 'center', height: '25px', fontSize: '25px', lineHeight: 1, margin: '0 auto' }}>Country Information</h1>
+        </div>
       </div>)}
     </AppContext.Consumer>
   )
